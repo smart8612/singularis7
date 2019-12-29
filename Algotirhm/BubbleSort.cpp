0927b3224f7bf4c9181d0acd2d5f07b45652b9d8
JeongTaek Han <smart8612@gmail.com>
2019-12-29 06:14:16 +0000

    BubbleSort

---
+++ Algotirhm/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+
+void BubbleSort(vi &vec) {
+	for (int i=0; i<vec.size(); i++) {
+		for (int j=0; j<vec.size()-i-1; j++) {
+			cout << vec[j] << ' ' << vec[j+1] << '\n';
+			if (vec[j] > vec[j+1]) {
+				swap(vec[j], vec[j+1]);
+			}
+		}
+	}
+}
+
+void printArray(vi &vec) {
+	cout << '[';
+	for (int i=0; i<vec.size(); i++) {
+		cout << vec[i];
+		(i != vec.size()-1)? cout << ',' : cout << ']';
+	}
+	cout << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	vi vec = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+	BubbleSort(vec);
+	printArray(vec);
+	
+	return 0;
+}
