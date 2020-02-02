a56cd51e4dc50b829e9eff1801df6310d475a76e
Han Jeong Taek <smart8612@gmail.com>
2020-02-02 20:41:51 +0900

    Update STL Vector Function Example.cpp

--- STL Example/STL Vector Function Example.cpp
+++ STL Example/STL Vector Function Example.cpp
@@ -15,35 +15,35 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-	cout << "Default vector status" << '\n';
+    cout << "Default vector status" << '\n';
     vi vec = {9, 2, 31, 1, 2, 51, 7, 1};
-	printArray(vec);
-	
-	cout << "Sorting By ASC" << '\n';
-	sort(vec.begin(), vec.end());
     printArray(vec);
 	
-	cout << "Reverse" << '\n';
-	reverse(vec.begin(), vec.end());
+    cout << "Sorting By ASC" << '\n';
+    sort(vec.begin(), vec.end());
+    printArray(vec);
+
+    cout << "Reverse" << '\n';
+    reverse(vec.begin(), vec.end());
     printArray(vec);
 	
-	cout << "Shuffling" << '\n';
-	random_shuffle(vec.begin(), vec.end());
-	printArray(vec);
+    cout << "Shuffling" << '\n';
+    random_shuffle(vec.begin(), vec.end());
+    printArray(vec);
 	
-	cout << "ReSort By ASC" << '\n';
-	sort(vec.begin(), vec.end());
+    cout << "ReSort By ASC" << '\n';
+    sort(vec.begin(), vec.end());
     printArray(vec);
     
-	cout << "Unique Bound Shuffling" << '\n';
-	random_shuffle(lower_bound(vec.begin(), vec.end(), 2), 
-				   upper_bound(vec.begin(), vec.end(), 20)
-				  );
-	printArray(vec);
+    cout << "Unique Bound Shuffling" << '\n';
+    random_shuffle(lower_bound(vec.begin(), vec.end(), 2), 
+                   upper_bound(vec.begin(), vec.end(), 20)
+                  );
+    printArray(vec);
 	
-	cout << "ReSort By ASC && Remove overlap" << '\n';
-	sort(vec.begin(), vec.end());
-	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+    cout << "ReSort By ASC && Remove overlap" << '\n';
+    sort(vec.begin(), vec.end());
+    vec.erase(unique(vec.begin(), vec.end()), vec.end());
     printArray(vec);
 	
     return 0;
