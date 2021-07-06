f6600b315f57e56ef42316a5e7f9a602fa16bebb
singularis7 <smart8612@gmail.com>
2021-07-06 21:51:48 +0900

    Create vectorbasic.cpp

---
+++ Algorithm/vectorbasic.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long C;
+typedef complex<C> P;
+
+#define X real()
+#define Y imag()
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    P p = {4, 2};
+    cout << p.X << ' ' << p.Y << '\n';
+
+    return 0;
+}
