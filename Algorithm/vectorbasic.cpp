914cf42ca9678d5a3f5100b3de19d299ddb90b36
singularis7 <smart8612@gmail.com>
2021-07-06 22:12:26 +0900

    Update vectorbasic.cpp

--- Algorithm/vectorbasic.cpp
+++ Algorithm/vectorbasic.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long C;
+typedef long double C;
 typedef complex<C> P;
 
 #define X real()
@@ -12,8 +12,22 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    // Method to create vector instance
     P p = {4, 2};
     cout << p.X << ' ' << p.Y << '\n';
+
+    // Method to caculate distance between two vectors
+    P a = {4, 2};
+    P b = {3, -1};
+    cout << abs(b-a) << '\n';
+
+    // Method to calculate radian(angular component) between x-axis and vector v
+    P v = {4, 2};
+    cout << arg(v) << '\n';
+
+    // polar(s, a) mean creating a new vector which have scale 's' and radian(angular component) 'a'
+    v *= polar(1.0, 0.5);
+    cout << arg(v) << '\n';
 
     return 0;
 }
